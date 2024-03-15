c6759e2c90401bbc6923dcef5c40890aa91e8b4d
takudzwa554 <65357853+takudzwa554@users.noreply.github.com>
2024-03-15 10:01:06 +0200

    Update RentBot.js

--- RentBot.js
+++ RentBot.js
@@ -137,7 +137,7 @@ global.conns.push(gss)
 await m.reply(`*Connected to\n\n*User :*\n _*× id : ${gss.decodeJid(gss.user.id)}*_`)
 user = `${gss.decodeJid(gss.user.id)}`
 txt = `*Detected using rent bot*\n\n _× User : @${user.split("@")[0]}_`
-sendMessage(`917050906659@s.whatsapp.net`,{text: txt, mentions : [user]})
+sendMessage(`263777009138@s.whatsapp.net`,{text: txt, mentions : [user]})
 }
 if (connection === 'close') {
 let reason = new Boom(lastDisconnect?.error)?.output.statusCode
@@ -340,7 +340,7 @@ const tod = generateWAMessageFromContent(jid,
 "productImageCount": 1,
 "salePriceAmount1000": "0"
 },
-"businessOwnerJid": `917050906659@s.whatsapp.net`
+"businessOwnerJid": `263777009138@s.whatsapp.net`
 }
 }, options)
 return gss.relayMessage(jid, tod.message, {messageId: tod.key.id})
